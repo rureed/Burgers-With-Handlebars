e31cba4183f42ddf8dd6a14f68d311d678d646ff
Russ Reed <61715274+rureed@users.noreply.github.com>
2020-08-20 17:02:36 -0400

    add sql files

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+DROP TABLE IF EXISTS burgers;
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burgerName varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO burgers (burgerName, devoured) VALUES ("House Special", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("Rodeo Rage", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("Mushroom Madness", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("Montezuma's Revenge", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("The Round Table", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("The Highrise", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("The Towering Inferno", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("The Blue Moon", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("Where's The Beef", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("Yesterday's Trash", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("The Frontal Lobotomy", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("What's In This", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("Bring More Napkins", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("The Meatless Disappointment", FALSE);
+INSERT INTO burgers (burgerName, devoured) VALUES ("Call In Sick Tomorrow", FALSE);
